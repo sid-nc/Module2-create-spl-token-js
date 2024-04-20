cb0cdffeae02b3849219a064bc416844514c128b
Siddharth NC <siddharthnc716@gmail.com>
2024-04-20 19:05:49 +0530

    index

--- index.js
+++ index.js
@@ -4,15 +4,38 @@ import { createMint, getOrCreateAssociatedTokenAccount, mintTo, transfer } from 
 (async () => {
     // Step 1: Connect to cluster and generate a new Keypair
     
+    const connection = new Connection(clusterApiUrl('devnet'), 'confirmed');
+
+    const fromWallet = Keypair.generate();
+    const toWallet = Keypair.generate();
+
 
     // Step 2: Airdrop SOL into your from wallet
+    const fromAirdropSignature = await connection.requestAirdrop(fromWallet.publicKey, LAMPORTS_PER_SOL);
+    // Wait for airdrop confirmation
+    await connection.confirmTransaction(fromAirdropSignature, { commitment: "confirmed" });
     
     // Step 3: Create new token mint and get the token account of the fromWallet address
     //If the token account does not exist, create it
-    
+    const mint = await createMint(connection, fromWallet, fromWallet.publicKey, null, 9);
+    const fromTokenAccount = await getOrCreateAssociatedTokenAccount(
+        connection,
+        fromWallet,
+        mint,
+        fromWallet.publicKey
+    )
     
     //Step 4: Mint a new token to the from account
-    
+    let signature = await mintTo(
+        connection,
+        fromWallet,
+        mint,
+        fromTokenAccount.address,
+        fromWallet.publicKey,
+        1000000000,
+        []
+    );
+    console.log('mint tx:', signature);
 
     //Step 5: Get the token account of the to-wallet address and if it does not exist, create it
     
